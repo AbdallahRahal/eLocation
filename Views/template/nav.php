6f6d2ebfa1a4dec83b637c3973129d6fe29a3c9a
Fopodir <abidi.feras@hotmail.com>
2018-11-13 20:29:24 +0100

    couleur orange

--- Views/template/nav.php
+++ Views/template/nav.php
@@ -9,6 +9,7 @@
             <a style="margin-left: -195px"class="nav-link dropdown-toggle" href="https://example.com/" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i style="font-size: 43px" class="fas fa-user-cog"></i> Mon compte</a>
             <div style="margin-left: -188px" class="dropdown-menu" aria-labelledby="dropdown01">
               <a class="dropdown-item" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" href=""> Se connecter</a>
+              <a class="dropdown-item" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" href=""> Paramètres du compte</a>
             </div>
           </li>
         </ul>
