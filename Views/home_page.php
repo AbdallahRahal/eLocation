9b206725551e346574dd431e14cb121c3398cd6b
Fopodir <abidi.feras@hotmail.com>
2018-11-13 18:02:25 +0100

    Carrousel

--- Views/home_page.php
+++ Views/home_page.php
@@ -1,6 +1,31 @@
 <main role="main" class="container">
 
-      <div class="home" style="margin-top: 172px;margin-left: 51px;text-align: center;">
-        <h1>eLocation</h1>
+      <div class="home" style="margin-top: 100px;margin-left: 51px;text-align: center;">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="Views/template/test.jpg" alt="Première slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="Views/template/test.jpg" alt="Seconde slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="Views/template/test.jpg" alt="Troisième slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Précédent</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Suivant</span>
+  </a>
+</div>
       </div>
 </main>
