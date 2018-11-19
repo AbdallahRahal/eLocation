cdd8dc047a82275663da9d271ed55c63044cd5b0
Fopodir <abidi.feras@hotmail.com>
2018-11-19 11:29:46 +0100

    fusion

---
+++ views/template/register_form.php
@@ -0,0 +1,43 @@
+<div class="modal fade" id="RegisterModal" tabindex="-1" role="dialog" aria-labelledby="RegisterModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="RegisterModalLabel"><a class="navbar-brand"><img src="views/template/eLocation.png" style="width: 30%"></a> </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h3>M'inscrire :</h3>
+        <form action="login" method="">
+          <div class="form-group">
+
+            <label for="recipient-name" class="col-form-label">Mail :</label>
+            <input type="text" class="form-control" name="pseudo" id="recipient-mail">
+
+          </div>
+          <div class="form-group">
+
+            <label for="recipient-name" class="col-form-label">pseudo :</label>
+            <input type="text" class="form-control" name="pseudo" id="recipient-pseudo">
+
+          </div>
+          <div class="form-group">
+
+            <label for="recipient-name" class="col-form-label">prenom :</label>
+            <input type="text" class="form-control" name="pseudo" id="recipient-prenom">
+
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">nom :</label>
+            <input type="password" class="form-control" name="mdp" id="message-nom">
+          </div>
+      </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-primary">S'inscrire</button>
+        </form>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+  </div>
+</div>
