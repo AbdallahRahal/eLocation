f024f153eafac205994fc472e6ba215f9890fa0d
Abdallah rahal <abdalah.rahal15@gmail.com>
2018-11-19 15:32:51 +0100

    inscription

--- controllers/handling_data/register.php
+++ controllers/handling_data/register.php
@@ -1,17 +1,22 @@
 <?php
 
-//include("models/get.php");
+include("models/get.php");
 
 foreach($_POST as $key => $value){
     if(empty($value)){  
 
         echo" <br> la ligne ".$key." est vide";
+        $test=false;
 
     }
 }
 
+if($test==false){
 
-    //$result =  connexion($_POST['identifiant'],$_POST['mdp']);
+    exit;
+}
+
+inscription($_POST['mail'],$_POST['pseudo'],$_POST['mdp'],$_POST['prenom'],$_POST['nom'],$_POST['adresse'],$_POST['sexe'],$_POST['cp'],$_POST['ville']);
 
 
 ?>
