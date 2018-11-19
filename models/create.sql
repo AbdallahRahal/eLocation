230dfe4fc4d14da01f8c71779ad742c1ef221663
ryan kerrad <Laieon@MacBook-Pro-de-yanis.local>
2018-11-19 11:24:30 +0100

    create.sql ok

--- models/create.sql
+++ models/create.sql
@@ -10,55 +10,55 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `mydb` ;
+DROP SCHEMA IF EXISTS `mydb`;
 
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8;
 USE `mydb` ;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`utilisateur`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`utilisateur` ;
+DROP TABLE IF EXISTS `mydb`.`utilisateur`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`utilisateur` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `pseudo` VARCHAR(45) NULL,
-  `mdp` VARCHAR(45) NULL,
-  `nom` VARCHAR(45) NULL,
-  `prenom` VARCHAR(45) NULL,
-  `adresse` VARCHAR(45) NULL,
-  `sexe` ENUM('H', 'F') NULL,
-  `mail` VARCHAR(45) NULL,
-  `cp` CHAR(5) NULL,
-  `ville` VARCHAR(45) NULL,
-  `statut` ENUM('utilisateur', 'admin') NULL,
-  `etat` ENUM('fidele', 'lambda') NULL,
+  `pseudo` VARCHAR(45) NOT NULL,
+  `mdp` VARCHAR(45) NOT NULL,
+  `nom` VARCHAR(45) NOT NULL,
+  `prenom` VARCHAR(45) NOT NULL,
+  `adresse` VARCHAR(45) NOT NULL,
+  `sexe` ENUM('H', 'F') NOT NULL,
+  `mail` VARCHAR(45) NOT NULL,
+  `cp` CHAR(5) NOT NULL,
+  `ville` VARCHAR(45) NOT NULL,
+  `statut` ENUM('utilisateur', 'admin') NOT NULL,
+  `etat` ENUM('fidele', 'lambda') NOT NULL,
   PRIMARY KEY (`id`))
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`article`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`article` ;
+DROP TABLE IF EXISTS `mydb`.`article`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`article` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NULL,
-  `description` VARCHAR(45) NULL,
-  `prix_journee` CHAR(5) NULL,
-  `lien_photo` VARCHAR(45) NULL,
-  `statut` ENUM('dispo', 'loue', 'reserve') NULL,
-  `etat` ENUM('neuf', 'abime') NULL,
+  `nom` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
+  `prix_journee` CHAR(5) NOT NULL,
+  `lien_photo` VARCHAR(45) NOT NULL,
+  `statut` ENUM('dispo', 'loue', 'reserve') NOT NULL,
+  `etat` ENUM('neuf', 'abime') NOT NULL,
   PRIMARY KEY (`id`))
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`action`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`action` ;
+DROP TABLE IF EXISTS `mydb`.`action`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`action` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -76,43 +76,32 @@ CREATE TABLE IF NOT EXISTS `mydb`.`action` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 
-<<<<<<< HEAD
 CREATE INDEX `fk_action_article1_idx` ON `mydb`.`action` (`article_id` ASC);
 
 CREATE INDEX `fk_action_utilisateur1_idx` ON `mydb`.`action` (`utilisateur_id` ASC);
-=======
-CREATE INDEX `fk_action_article1_idx` ON `mydb`.`action` (`article_id` ASC) ;
-
-CREATE INDEX `fk_action_utilisateur1_idx` ON `mydb`.`action` (`utilisateur_id` ASC) ;
->>>>>>> 8accc82093c06fbe997313e85a847328b3afca80
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`point_relais`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`point_relais` ;
+DROP TABLE IF EXISTS `mydb`.`point_relais`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`point_relais` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NULL,
-  `adresse` VARCHAR(45) NULL,
-  `horaire` VARCHAR(45) NULL,
-  `cp` CHAR(5) NULL,
-  `ville` VARCHAR(45) NULL,
+  `nom` VARCHAR(45) NOT NULL,
+  `adresse` VARCHAR(45) NOT NULL,
+  `horaire` VARCHAR(45) NOT NULL,
+  `cp` CHAR(5) NOT NULL,
+  `ville` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
 
-<<<<<<< HEAD
 CREATE UNIQUE INDEX `adresse_UNIQUE` ON `mydb`.`point_relais` (`adresse` ASC);
-=======
-CREATE UNIQUE INDEX `adresse_UNIQUE` ON `mydb`.`point_relais` (`adresse` ASC) ;
->>>>>>> 8accc82093c06fbe997313e85a847328b3afca80
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`louer`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`louer` ;
+DROP TABLE IF EXISTS `mydb`.`louer`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`louer` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -135,21 +124,15 @@ CREATE TABLE IF NOT EXISTS `mydb`.`louer` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 
-<<<<<<< HEAD
 CREATE INDEX `fk_location_action1_idx` ON `mydb`.`louer` (`action_id` ASC);
 
 CREATE INDEX `fk_location_point_relais1_idx` ON `mydb`.`louer` (`point_relais_id` ASC);
-=======
-CREATE INDEX `fk_location_action1_idx` ON `mydb`.`louer` (`action_id` ASC)  ;
-
-CREATE INDEX `fk_location_point_relais1_idx` ON `mydb`.`louer` (`point_relais_id` ASC)  ;
->>>>>>> 8accc82093c06fbe997313e85a847328b3afca80
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`avis`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`avis` ;
+DROP TABLE IF EXISTS `mydb`.`avis`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`avis` (
   `id` INT NOT NULL,
@@ -163,11 +146,11 @@ CREATE TABLE IF NOT EXISTS `mydb`.`avis` (
 -- -----------------------------------------------------
 -- Table `mydb`.`categorie`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`categorie` ;
+DROP TABLE IF EXISTS `mydb`.`categorie`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`categorie` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NULL,
+  `nom` VARCHAR(45) NOT NULL,
   `promo` INT NULL,
   PRIMARY KEY (`id`))
 
@@ -175,7 +158,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`categorie` (
 -- -----------------------------------------------------
 -- Table `mydb`.`appartenir`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`appartenir` ;
+DROP TABLE IF EXISTS `mydb`.`appartenir`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`appartenir` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -193,19 +176,19 @@ CREATE TABLE IF NOT EXISTS `mydb`.`appartenir` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 
-CREATE INDEX `fk_appartenir_categorie_idx` ON `mydb`.`appartenir` (`categorie_id` ASC)  ;
+CREATE INDEX `fk_appartenir_categorie_idx` ON `mydb`.`appartenir` (`categorie_id` ASC);
 
-CREATE INDEX `fk_appartenir_article1_idx` ON `mydb`.`appartenir` (`article_id` ASC)  ;
+CREATE INDEX `fk_appartenir_article1_idx` ON `mydb`.`appartenir` (`article_id` ASC);
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`vendre`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`vendre` ;
+DROP TABLE IF EXISTS `mydb`.`vendre`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`vendre` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `date_vente` DATE NULL,
+  `date_vente` DATE NOT NULL,
   `action_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_vente_action1`
@@ -214,18 +197,18 @@ CREATE TABLE IF NOT EXISTS `mydb`.`vendre` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 
-CREATE INDEX `fk_vente_action1_idx` ON `mydb`.`vendre` (`action_id` ASC)  ;
+CREATE INDEX `fk_vente_action1_idx` ON `mydb`.`vendre` (`action_id` ASC);
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`num_proposition`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`num_proposition` ;
+DROP TABLE IF EXISTS `mydb`.`num_proposition`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`num_proposition` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NULL,
-  `date_propo` DATE NULL,
+  `nom` VARCHAR(45) NOT NULL,
+  `date_propo` DATE NOT NULL,
   `utilisateur_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_num_proposition_utilisateur1`
@@ -234,21 +217,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`num_proposition` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 
-CREATE INDEX `fk_num_proposition_utilisateur1_idx` ON `mydb`.`num_proposition` (`utilisateur_id` ASC)  ;
+CREATE INDEX `fk_num_proposition_utilisateur1_idx` ON `mydb`.`num_proposition` (`utilisateur_id` ASC);
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`ligne_proposition`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`ligne_proposition` ;
+DROP TABLE IF EXISTS `mydb`.`ligne_proposition`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`ligne_proposition` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NULL,
-  `prix` INT NULL,
-  `description` VARCHAR(45) NULL,
-  `photo` BLOB NULL,
-  `stade` ENUM('proposition', 'offre', 'valide') NULL,
+  `nom` VARCHAR(45) NOT NULL,
+  `prix` INT NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
+  `photo` BLOB NOT NULL,
+  `stade` ENUM('proposition', 'offre', 'valide') NOT NULL,
   `num_proposition_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_ligne_proposition_num_proposition1`
@@ -257,13 +240,13 @@ CREATE TABLE IF NOT EXISTS `mydb`.`ligne_proposition` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 
-CREATE INDEX `fk_ligne_proposition_num_proposition1_idx` ON `mydb`.`ligne_proposition` (`num_proposition_id` ASC)  ;
+CREATE INDEX `fk_ligne_proposition_num_proposition1_idx` ON `mydb`.`ligne_proposition` (`num_proposition_id` ASC);
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`ligne_vente`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`ligne_vente` ;
+DROP TABLE IF EXISTS `mydb`.`ligne_vente`;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`ligne_vente` (
    `id` INT NOT NULL AUTO_INCREMENT,
