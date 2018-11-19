1a58a4cea41b526fb6df39ca5d3f4037bf5d5d7a
Abdallah rahal <abdalah.rahal15@gmail.com>
2018-11-19 15:49:38 +0100

    a

--- models/requete.php
+++ models/requete.php
@@ -17,4 +17,12 @@ function name() {
     return($name);
 }
 
+
+function inscription($mail,$pseudo,$mdp,$prenom,$nom,$adresse_post,$sexe,$cp,$ville) {
+    include("../../models/db_connect.php");
+    $req = $bdd->prepare("INSERT INTO utilisateur (mail,pseudo,mdp,prenom,nom,adresse,sexe,cp,ville,statut,etat) Values(:mail,:pseudo,:mdp,:prenom,:nom,:adresse_post,:sexe,:cp,:ville,utilisateur,lambda) ");
+    $req-> execute(array(":mail"=> $mail, ":pseudo" =>$pseudo, ":mdp" =>$mdp, ":prenom" =>$prenom, ":nom" =>$nom, ":adresse_post" =>$adresse_post, ":sexe" =>$sexe, ":cp" =>$cp, ":ville" =>$ville) );
+
+}
+
 ?>
