8413699363f4ebc0a8dab6c9e1e5b88faaf61d1c
Isma49 <amazigh_trz@yahoo.fr>
2018-11-19 16:26:39 +0100

    fusion finie

--- controllers/handling_data/login.php
+++ controllers/handling_data/login.php
@@ -5,14 +5,20 @@ if(empty($_POST['identifiant'])){
 }elseif(empty($_POST['mdp'])){ 
 
 }else{
+	include_once 'models/requete.php';
     $result =  connexion($_POST['identifiant'],$_POST['mdp']);
     if($result != NULL){
         $_SESSION['compte']= $result['statut'];
-        $_SESSION['pseudo'] = $_SESSION['pseudo'];
-        exit(); 
-    }else{
-        echo"erreur le compte n'existe pas";
-    }
+        $_SESSION['pseudo'] = $result['pseudo'];         
+    }else{ 
+			require_once 'views/template/login_form.php'; ?>
+
+		<script> 
+			$('#exampleModal').modal('show');
+			$('#alerterror').show();
+		</script>
+	<?php	
+	 }
 }
 
 ?>
--- controllers/redirection_user.php
+++ controllers/redirection_user.php
@@ -1,29 +1,37 @@
 <?php 
 session_start();
 
-include 'views/template/nav.php';
-include 'views/template/rubrique.php';
-include('models/requete.php');
-if(isset($_POST['connexion'])){
-	include('controllers/handling_data/login.php');
+
+
+if(isset($_POST['connexion'])) {
+
+	include_once('controllers/handling_data/login.php');
+
 }
 
-if(isset($_POST['compte'])) {
+if(isset($_SESSION['compte'])) {
+	
 	include 'views/template/settings_form.php';
+
 }else{
+	
 	include 'views/template/login_form.php';
 	include 'views/template/register_form.php';
+
 }
 
+include 'views/template/nav.php';
+include 'views/template/rubrique.php';
+//include 'models/requete.php';
 
 if(!isset($_SESSION['compte']) || $_SESSION['compte'] == 'utilisateur' ) {
 	$rubrique=array("cat"=>"Catégorie","loc"=>"Mes Locations","vendre"=>"Vendre");
 	rubriques($rubrique);
 	if($_GET['rub'] == 'cat' ) {
 		echo "Ici, on affiche les catégories";
-	}elseif ($_GET['rub'] == 'loc') {
+	} elseif ($_GET['rub'] == 'loc') {
 		echo "ici, seront visible nos loc";
-	}elseif ($_GET['rub'] == 'vendre') { 
+	} elseif ($_GET['rub'] == 'vendre') { 
 		include 'views/div/form_proposition_vente.php';
 	}
 	if(isset($_SESSION['compte'])){
