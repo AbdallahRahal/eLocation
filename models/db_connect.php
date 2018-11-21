068c931db43d314d54dc709fcc0181b91cd449fb
Fopodir <abidi.feras@hotmail.com>
2018-11-21 14:14:05 +0100

    db_connect

--- models/db_connect.php
+++ models/db_connect.php
@@ -1,7 +1,3 @@
 <?php
-<<<<<<< HEAD
-$bdd = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', 'root');
-=======
-$bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'root');
->>>>>>> 1f0ef76325aff70231e64dc6670f864a1d916c10
+$bdd = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', '');
 ?>
