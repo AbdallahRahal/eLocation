e3fb998f042e010737e8a68d01ba61ca36c0acc5
Abdallah rahal <abdalah.rahal15@gmail.com>
2018-12-19 14:54:21 +0100

    ajout prix reprise

--- views/template/ajout_article.php
+++ views/template/ajout_article.php
@@ -1,7 +1,7 @@
 <?php while($donneesAffichage = $affichage_ajout->fetch()){ ?>
 
 <div class="container" style="max-width: 50%;margin-top: 6%">
-<h3>Ajout de l'article <?=$donneesAffichage['ID']?>:</h3>
+<h3>Ajout de l'article <?=$donneesAffichage['Nom']?>:</h3>
     <form action="index.php" method="GET">
     <input type='hidden' name='page' value='accueil'>
           <input type='hidden' name='rub' value='reprises'>
@@ -13,7 +13,7 @@
           </div>
           <div class="form-group">
 
-            <label for="recipient-name" class="col-form-label">Prix : <?=$donneesAffichage['Prix']?></label>
+            <label for="recipient-name" class="col-form-label">Prix : <input required type='number' value=<?=$donneesAffichage['Prix']?>></label>
 
           </div>
           <div class="form-group">
