cd19c056fb5f77ee64bc131344c22d942aa2077e
Fopodir <abidi.feras@hotmail.com>
2018-12-20 20:47:28 +0100

    vendre

--- views/div/form_proposition_vente.php
+++ views/div/form_proposition_vente.php
@@ -1,18 +1,17 @@
 <div class="container" style="max-width: 60%;margin-top: 8%">
 <form action="" method="GET" action="page.php" enctype="multipart/form-data">
-    <input  type='hidden' name='page' value='accueil' >
-    <input  type='hidden' name='rub' value='proposition' >
+    <input type='hidden' name='page' value='accueil' >
+    <input type='hidden' name='rub' value='proposition' >
     <div class="form-group">
-
         <label for="exampleFormControlTextarea1">Titre de l'annonce :</label>
-        <input class="form-control" type="text" name="titre">   
+        <input required class="form-control" type="text" name="titre">   
     </div>
     <div class="form-group">
         <label for="exampleFormControlTextarea1">Texte de l'annonce :</label>
-        <textarea placeholder="Ici vous pouvez faire une proposition de reprise a un administrateur(qui est beau goss), cette administrateur vous feras en retour une offre de reprise" class="form-control" id="exampleFormControlTextarea1" rows="2" name="description"></textarea>
+        <textarea required placeholder="Ici vous pouvez faire une proposition de reprise a un administrateur, cette administrateur vous feras en retour une offre de reprise" class="form-control" id="exampleFormControlTextarea1" rows="2" name="description"></textarea>
     </div>
     <label for="file" class="label-file" style="max-width: 15%;"><p>Image principale :</p><p><img src="views/template/photo.png" style="height:14%;"></p></label>
-    <input id="file" class="input-file" type="file" name="img1">
+    <input required id="file" class="input-file" type="file" name="img1">
     <p><button class="btn btn-primary" name="proposition" value="true" type="submit">Envoyer</button></p>
     <div class="form-group">
 </form>
