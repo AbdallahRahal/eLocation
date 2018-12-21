52a705db9e4375112c931a0f8eaecd33beb7daf8
Abdallah rahal <abdalah.rahal15@gmail.com>
2018-12-21 10:29:06 +0100

    mailer

---
+++ views/template/avis.php
@@ -0,0 +1,18 @@
+<div class="" style="margin-left: 20%;margin-top: 100px">
+<p><h2>Laisser un avis</h2></p>
+
+<form action="" method="get">
+Commentaire : <br>
+<input required type='text' name="commentaire">
+<br>Note<br>
+<select name='note'>
+    <option value=0>0</option>
+    <option value=1>1</option>
+    <option value=2>2</option>
+    <option value=3>3</option>
+    <option value=4>4</option>
+    <option value=5>5</option>
+</select>
+<button type='submit' value='true' name='avis'>Envoyer</button>
+</form>
+</div>
