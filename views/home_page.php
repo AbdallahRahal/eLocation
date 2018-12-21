d1c6e35a494d96cff4e8c741a16d20ba16e43bc7
Isma49 <amazigh_trz@yahoo.fr>
2018-12-21 14:42:06 +0100

    it2

--- views/home_page.php
+++ views/home_page.php
@@ -20,19 +20,19 @@
         <br><h2>Les derniers articles ajoutés</h2><br>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="<?=$lien_photo[1]?>" alt="Première slide" style="max-width: 40%;margin-left: 30%;margin-top: 5%;">
+      <img class="d-block w-100" src="views/img/<?=$lien_photo[1]?>" alt="Première slide" style="max-width: 40%;margin-left: 30%;margin-top: 5%;">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="<?=$lien_photo[2]?>" alt="Seconde slide" style="max-width: 40%;margin-left: 30%;margin-top: 5%;">
+      <img class="d-block w-100" src="views/img/<?=$lien_photo[2]?>" alt="Seconde slide" style="max-width: 40%;margin-left: 30%;margin-top: 5%;">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="<?=$lien_photo[3]?>" alt="Troisième slide" style="max-width: 40%;margin-left: 30%;margin-top: 5%;">
+      <img class="d-block w-100" src="views/img/<?=$lien_photo[3]?>" alt="Troisième slide" style="max-width: 40%;margin-left: 30%;margin-top: 5%;">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="<?=$lien_photo[4]?>" alt="Quatrième slide" style="max-width: 40%;margin-left: 30%;margin-top: 5%;">
+      <img class="d-block w-100" src="views/img/<?=$lien_photo[4]?>" alt="Quatrième slide" style="max-width: 40%;margin-left: 30%;margin-top: 5%;">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="<?=$lien_photo[5]?>" alt="Cinquième slide" style="max-width: 40%;margin-left: 30%;margin-top: 5%;">
+      <img class="d-block w-100" src="views/img/<?=$lien_photo[5]?>" alt="Cinquième slide" style="max-width: 40%;margin-left: 30%;margin-top: 5%;">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
