1c762801ba66057cf38f5f53bd52ba7517afb1b7
Abdallah rahal <abdalah.rahal15@gmail.com>
2019-01-27 18:08:32 +0100

    modifier ses commentaire et les supprimer && supprimer les com pr l'admin

--- controllers/redirection_user.php
+++ controllers/redirection_user.php
@@ -62,7 +62,9 @@ if(!isset($_SESSION['compte']) || $_SESSION['compte'] == 'utilisateur' ) {
 					affichage_article($affiche);
 				
 				}else{
-			
+					if(isset($_POST['valider_modif_commentaire']) || isset($_POST['suppr_commentaire'] )) {
+						modif_commentaire($_POST);
+					}
 					include 'views/div/affichage_page_vente.php';
 					$article = info_article($_GET['art']);
                     $commentaire = commentaire_article($_GET['art']);
@@ -80,6 +82,9 @@ if(!isset($_SESSION['compte']) || $_SESSION['compte'] == 'utilisateur' ) {
 						}
 						 
 					}else{
+						if(isset($_POST['valider_modif_commentaire']) || isset($_POST['suppr_commentaire'] )) {
+							modif_commentaire($_POST);
+						}
 						include 'views/div/affichage_page_vente.php';
 						$article = info_article($_GET['art']);
 						$commentaire = commentaire_article($_GET['art']);
@@ -137,6 +142,9 @@ if(!isset($_SESSION['compte']) || $_SESSION['compte'] == 'utilisateur' ) {
 			
 				}else{
 		
+					if(isset($_POST['valider_modif_commentaire']) || isset($_POST['suppr_commentaire'] )) {
+						modif_commentaire($_POST);
+					}
 					include 'views/div/affichage_page_vente.php';
 					$article = info_article($_GET['art']);
 					$suppr_com=suppr_com();
